aa5ac8dbbdd1317b89113a54235cbd7e83865698
Mai297 <58828261+Mai297@users.noreply.github.com>
2019-12-20 22:39:27 +0200

    Add files via upload

---
+++ jsonn.js
@@ -0,0 +1,84 @@
+data=[
+    {
+        //"name":"q1",
+        "header":"1-",
+        "title":"the woman who gave birth to me is my father",
+        "answer1":"true",
+        "answer2":"false",
+        "correctAnswer":true
+        },
+    {
+        //"name":"q2",
+        "header":"2-",
+        "title":"I am aboy I am my father,s son",
+        "answer1":"true",
+        "answer2":"false",
+        "correctAnswer":true
+    },
+    {
+    //"name":"q3",
+    "header":"3-",
+    "title":"the mother of my mother is my grand father",
+    "answer1":"true",
+    "answer2":"false",
+    "correctAnswer":true
+    },
+    {
+    //"name":"q4",
+    "header":"4-",
+    "title":"the brother of my father is my aunt",
+    "answer1":"true",
+    "answer2":"false",
+    "correctAnswer":true
+    },
+    {
+        //"name":"q5",
+        "header":"5-",
+        "title":"I am agirl.I am dating aboy. he is my fiance",
+        "answer1":"true",
+        "answer2":"false",
+        "correctAnswer":true
+        },
+        {
+           // "name":"q6",
+            "header":"6-",
+            "title":"My sister has adauter.she is my niece",
+            "answer1":"true",
+            "answer2":"false",
+            "correctAnswer":true
+            },
+            {
+                //"name":"q7",
+                "header":"7-",
+                "title":"My mom is my father,s husband",
+                "answer1":"true",
+                "answer2":"false",
+                "correctAnswer":true
+                },
+                {
+                    //"name":"q8",
+                    "header":"8-",
+                    "title":"My sister has got married and now I have my brother-in-low",
+                    "answer1":"true",
+                    "answer2":"false",
+                    "correctAnswer":true
+                    },
+                    {
+                        //"name":"q9",
+                        "header":"9-",
+                        "title":"I am agirl I am my Father,s son",
+                        "answer1":"true",
+                        "answer2":"false",
+                        "correctAnswer":true
+                        },
+                        {
+                           // "name":"q10",
+                            "header":"10-",
+                            "title":"my uncle,s son is my niece",
+                            "answer1":"true",
+                            "answer2":"false",
+                            "correctAnswer":true
+                            }
+                        ]
+                    
+                      
